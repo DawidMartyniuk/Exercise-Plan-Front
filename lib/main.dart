b94a20d09f64160902a66fb41fe2c968fb9361d4
Dawid <dawidmartyniuk1@gmail.com>
2025-12-28 16:30:32 +0100

    big fix

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:work_plan_front/app.dart';
 import 'package:work_plan_front/core/app_initializer.dart';
-///TODO: Przechowywanie danych w pamięci lokalnej – zapewnia dostęp do danych bez połączenia z Internetem.
-///TODO: Synchronizacja z serwerem – automatyczne aktualizowanie danych po odzyskaniu połączenia.
-///TODO: Obsługa offline – możliwość korzystania z podstawowych funkcjonalności bez Internetu
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
